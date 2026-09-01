cfd12c75e000457bb130503c1c7c3610b0be000c
Friday <fadonic@yahoo.com>
2026-09-01 16:50:51 +0100

    Refactor greet() into Greeter class with locale support

--- greeter.js
+++ greeter.js
@@ -1,6 +1,17 @@
-function greet (name, title) {
-  const prefix = title ? title + ' ' : ''
-  return 'Hello, ' + prefix + name + '!'
+class Greeter {
+  constructor (locale = 'en') {
+    this.locale = locale
+  }
+
+  greet (name) {
+    const greetings = { en: 'Hello', es: 'Hola', fr: 'Bonjour' }
+    return `${greetings[this.locale] || greetings.en}, ${name}!`
+  }
+
+  farewell (name) {
+    const farewells = { en: 'Goodbye', es: 'Adiós', fr: 'Au revoir' }
+    return `${farewells[this.locale] || farewells.en}, ${name}!`
+  }
 }
 
-module.exports = { greet }
+module.exports = { Greeter }
